d96be5ef46f29844069c313a9b2a93d9d490b59f
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2024-09-01 22:53:36 +0200

    Plunge Attack

--- Assets/Scripts/PlayerInputManager.cs
+++ Assets/Scripts/PlayerInputManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -9,18 +10,21 @@ using UnityEngine.InputSystem;
 public class PlayerInputManager : MonoBehaviour
 {
 	[Tooltip("Height of first jump")]
-	[SerializeField] float jumpHeight = 3f;
+	[SerializeField] float jumpHeight = 12f;
 	[Tooltip("Speed")]
 	[SerializeField] float moveSpeed = 4f;
 
-	private const float gravityValue = -9.81f;
-	private const float jumpGravityScale = 0.5f;
+	private float gravityValue = -9.81f;
+	[SerializeField]private float jumpGravityScale = 2f;
+	private float fallGravityScale = 1f;
 
 	private PlayerInput playerInput;
     private CharacterController controller;
 
 	private Vector3 playerVelocity;
 	private bool groundedPlayer;
+
+	private uint numberOfJumps = 2; 
 
 	private void Awake()
 	{
@@ -30,7 +34,6 @@ public class PlayerInputManager : MonoBehaviour
 	private void OnEnable()
 	{
 		playerInput.actions["Jump"].performed += Jump;
-		playerInput.actions["Move"].performed += Move;
 	}
 
 	void Start()
@@ -47,12 +50,15 @@ public class PlayerInputManager : MonoBehaviour
 		if (playerVelocity.y > 0)
 		{
 			playerVelocity.y += gravityValue * jumpGravityScale * Time.deltaTime;
+			Debug.Log(playerVelocity.y);
 		}
 		else if(!groundedPlayer)
 		{
-			playerVelocity.y += gravityValue * Time.deltaTime;
-			Debug.Log(playerVelocity.y);
+			playerVelocity.y += gravityValue * fallGravityScale * Time.deltaTime;
 		}
+
+		Vector2 input = playerInput.actions["Move"].ReadValue<Vector2>();
+		playerVelocity = new Vector3(input.x * moveSpeed, playerVelocity.y, input.y * moveSpeed);
 
 		controller.Move(playerVelocity * Time.deltaTime);
 		playerVelocity.x = 0;
@@ -60,23 +66,27 @@ public class PlayerInputManager : MonoBehaviour
 
 		if(groundedPlayer)
 		{
-			playerInput.SwitchCurrentActionMap("Ground");
-			playerVelocity.y = 0;
-		}
-	}
+			playerInput.actions["PlungeAttack"].performed -= PlungeAttack;
 
-	private void Move(InputAction.CallbackContext context)
-	{
-		Vector2 input = playerInput.actions["Move"].ReadValue<Vector2>();
-		playerVelocity = new Vector3(input.x * moveSpeed, playerVelocity.y, input.y * moveSpeed);
-		Debug.Log("mooove");
+			fallGravityScale = 1f;
+			playerVelocity.y = 0;
+
+			numberOfJumps = 2;
+		}
+		else
+		{
+			playerInput.actions["Attack"].performed += Attack;
+			playerInput.actions["PlungeAttack"].performed += PlungeAttack;
+		}
 	}
 
 	private void Jump(InputAction.CallbackContext context)
 	{
-		playerInput.SwitchCurrentActionMap("Air");
-		playerVelocity.y = 1f * jumpHeight;
-		playerInput.actions["Attack"].performed += Attack;
+		if(groundedPlayer || numberOfJumps > 0)
+		{
+			numberOfJumps--;
+			playerVelocity.y += 1f * jumpHeight;
+		}
 	}
 
 	private void Attack(InputAction.CallbackContext context) 
@@ -88,7 +98,25 @@ public class PlayerInputManager : MonoBehaviour
 	private IEnumerator PerformAttack()
 	{
 		playerVelocity = Vector3.zero;
-		yield return new WaitForSeconds(1f);
+		gravityValue = 0f;
+		float duration = 0.8f;
+		float currentTime = 0f;
+		while (currentTime < duration)
+		{
+			float progress = currentTime / duration;
+			transform.rotation = Quaternion.AngleAxis(Mathf.Lerp(transform.rotation.eulerAngles.y, 360, progress), Vector3.forward);
+			currentTime += Time.deltaTime;
+			yield return new WaitForFixedUpdate();
+		}
+		transform.rotation = Quaternion.identity;
+		gravityValue = -9.81f;
+		yield return null;	
+	}
 
+	private void PlungeAttack(InputAction.CallbackContext context) 
+	{
+		Debug.Log("Plunging");
+		//playerVelocity.y = -10f;
+		fallGravityScale = 15f;
 	}
 }
